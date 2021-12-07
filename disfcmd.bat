1866b0feb71826b2d169d3ec0a15307779e803cc
GitHubCat-dev <70247964+GitHubCat-dev@users.noreply.github.com>
2021-12-07 20:38:27 +0700

    Update disfcmd.bat

--- disfcmd.bat
+++ disfcmd.bat
@@ -1,36 +1,3 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAnk
-::fBw5plQjdG8=
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSDk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+JeA==
-::cxY6rQJ7JhzQF1fEqQJQ
-::ZQ05rAF9IBncCkqN+0xwdVs0
-::ZQ05rAF9IAHYFVzEqQJQ
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFCxdQgCNK3iGKbsZzPry+e+7hkgOX/YwaLPv/4e6bsUrzyU=
-::YB416Ek+ZG8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 @echo off
 setlocal
 SET /P AREYOUSURE=Running this script may require you to reboot your computer. Make sure to save any open files, close any open programs, and have elevated privileges. Proceed?(Y/N)
@@ -39,12 +6,16 @@ dism /online /cleanup-image /restorehealth
 if errorlevel 0 
 (
 sfc /scannow
-if not errorlevel 0 
 (
-echo Error. Run SFC in Safe Mode to try again.
+if errorlevel 0
+(
+SET /P AREYOUSURE=SFC has finished repairing your system files. Do you want to reboot your computer now?(Y/N)
+IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
+shutdown -r -t 0
 )
 )
-if errorlevel 1 
+)
+if not errorlevel 0
 (
 @echo off
 setlocal
